97c1f63cfd189c25cd0e81279c7dc303abb21a83
yeosang0508 <yeosang0508@gmail.com>
2024-06-19 00:06:24 +0900

    ex1_0618_사람이 a왼팔 이라는 변수를 가지게

---
+++ src/main/java/assignment_0618/ex1_0618.java
@@ -0,0 +1,21 @@
+package assignment_0618;
+
+// 문제 : 아래가 실행되도록 해주세요.
+
+public class ex1_0618 {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔();
+    }
+}
+class 사람 {
+    // 클래스안에 변수를 정의 할 수 있다.
+    int 나이;
+    팔 a왼팔;
+}
+
+class 팔 {
+
+}
