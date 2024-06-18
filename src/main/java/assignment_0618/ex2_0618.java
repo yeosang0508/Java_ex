78fb956ee3efb9286c1cfaff2cb1485e4d2767a0
yeosang0508 <yeosang0508@gmail.com>
2024-06-19 00:57:53 +0900

    서로 다른 공격형태

--- src/main/java/assignment_0618/ex2_0618.java
+++ src/main/java/assignment_0618/ex2_0618.java
@@ -1,30 +1,30 @@
-package assignment_0618;
-// 문제 : 아래가 실행되도록 해주세요.
-
-public class ex2_0618 {
-    public static void main(String[] args) {
-        전사 a전사 = new 전사();
-
-        a전사.나이 = 20;
-        a전사.a무기 = new 활();
-        a전사.a무기 = new 칼();
-    }
-}
-
-class 전사 {
-    int 나이;
-    무기 a무기;
-
-
-}
-class 무기 {
-
-}
-
-class 활 extends 무기{
-
-}
-class 칼 extends 무기{
-
-}
+//package assignment_0618;
+//// 문제 : 아래가 실행되도록 해주세요.
+//
+//public class ex2_0618 {
+//    public static void main(String[] args) {
+//        전사 a전사 = new 전사();
+//
+//        a전사.나이 = 20;
+//        a전사.a무기 = new 활();
+//        a전사.a무기 = new 칼();
+//    }
+//}
+//
+//class 전사 {
+//    int 나이;
+//    무기 a무기;
+//
+//
+//}
+//class 무기 {
+//
+//}
+//
+//class 활 extends 무기{
+//
+//}
+//class 칼 extends 무기{
+//
+//}
 
